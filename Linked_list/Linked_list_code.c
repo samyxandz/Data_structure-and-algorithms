f1ebebb3c19621e00d990f6be7a1652e5e5c14af
Tamaghna Choudhuri <tamaghna.official@gmail.com>
2023-04-21 17:37:01 +0000

    changes

--- Linked_list/Linked_list_code.c
+++ Linked_list/Linked_list_code.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
-struct stud {
-int roll;
-char name[25];
-int age;
-struct stud *next;
-};
+    struct stud {
+    int roll;
+    char name[25];
+    int age;
+    struct stud *next;
+    };
 
 /* A user-defined data type called “node” */
 typedef struct stud node;
@@ -35,7 +35,7 @@ node *create_list()
 
 //function to traverse a list
 void display (node *head)
-{
+{    
     int count = 1;
     node *p;
     p = head;
@@ -47,6 +47,44 @@ void display (node *head)
         p = p->next;
     }
     printf ("\n");
+}
+
+void insert (node **head)
+
+{
+    int k = 0, rno;
+    node *p, *q, *new;
+    new = (node *) malloc(sizeof(node));
+    printf ("\nData to be inserted: ");
+    scanf ("%d %s %d", &new->roll, new->name, &new->age);
+    printf ("\nInsert before roll w:");
+    scanf ("%d", &rno);
+    p = *head;
+    if (p->roll == rno) /* At the beginning */
+    {
+        new->next = p;
+        *head = new;
+    }
+    else
+    {
+        while ((p != NULL) && (p->roll != rno))
+        {
+        q = p;
+        p = p->next;
+        }
+        if (p == NULL) /* At the end */
+        {
+        q->next = new;
+        new->next = NULL;
+        }
+        else if (p->roll == rno)
+        {
+        q->next = new;
+        new->next = p;
+        }
+
+    }
+
 }
 
 int main()
