b99bbcddd8b15f98aa9158adf11f1d4d55fd6173
Tamaghna Choudhuri <tamaghna.official@gmail.com>
2023-05-31 15:32:07 +0000

    31-05-2023

---
+++ graph/code_snippets/adj-matrix_to_adj-list.cpp
@@ -0,0 +1,12 @@
+vector<int> adjLs[V]; 
+        
+        // to change adjacency matrix to list 
+        for(int i = 0;i<V;i++) {
+            for(int j = 0;j<V;j++) {
+                // self nodes are not considered
+                if(adj[i][j] == 1 && i != j) {
+                    adjLs[i].push_back(j); 
+                    adjLs[j].push_back(i); 
+                }
+            }
+        }
